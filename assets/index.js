dd7937fc3a18aaa71f1fcffaa536fd2041ef4bb5
42343234234 <potrzebnye1@gmail.com>
2025-04-15 23:55:42 +0200

    Update index.js

--- assets/index.js
+++ assets/index.js
@@ -5,84 +5,103 @@ imageInput.type = "file";
 imageInput.accept = ".jpeg,.png,.gif";
 
 document.querySelectorAll(".input_holder").forEach((element) => {
+
     var input = element.querySelector(".input");
     input.addEventListener('click', () => {
         element.classList.remove("error_shown");
-    });
+    })
+
 });
 
 upload.addEventListener('click', () => {
     imageInput.click();
 });
 
-imageInput.addEventListener('change', () => {
+imageInput.addEventListener('change', (event) => {
+
     upload.classList.remove("upload_loaded");
     upload.classList.add("upload_loading");
-    upload.removeAttribute("selected");
+
+    upload.removeAttribute("selected")
 
     var file = imageInput.files[0];
     var data = new FormData();
     data.append("image", file);
 
-    fetch('https://api.imgur.com/3/image', {
+    fetch('	https://api.imgur.com/3/image' ,{
         method: 'POST',
         headers: {
-            'Authorization': 'Client-ID 74fad879e8270d9'
+            'Authorization': 'Client-ID 61ae1a8480a41ea'
         },
         body: data
     })
     .then(result => result.json())
     .then(response => {
+        
         var url = response.data.link;
-
-        upload.classList.remove("error_shown");
+        upload.classList.remove("error_shown")
         upload.setAttribute("selected", url);
         upload.classList.add("upload_loaded");
         upload.classList.remove("upload_loading");
+        upload.querySelector(".upload_uploaded").src = url;
 
-        const img = upload.querySelector(".upload_uploaded");
-        img.src = url;
-        img.style.display = "block";
-    });
-});
+    })
+
+})
 
 document.querySelector(".go").addEventListener('click', () => {
+
     var empty = [];
+
     var params = new URLSearchParams();
 
-    if (!upload.hasAttribute("selected")) {
+    if (!upload.hasAttribute("selected")){
         empty.push(upload);
-        upload.classList.add("error_shown");
-    } else {
+        upload.classList.add("error_shown")
+    }else{
         params.append("image", upload.getAttribute("selected"));
     }
 
     document.querySelectorAll(".input_holder").forEach((element) => {
+
         var input = element.querySelector(".input");
         params.append(input.id, input.value);
 
-        if (isEmpty(input.value)) {
+        if (isEmpty(input.value)){
             empty.push(element);
             element.classList.add("error_shown");
         }
-    });
 
-    if (empty.length !== 0) {
+    })
+
+    if (empty.length != 0){
         empty[0].scrollIntoView();
-    } else {
+    }else{
         forwardToId(params);
     }
+
 });
 
-function isEmpty(value) {
-    return /^\s*$/.test(value);
+function isEmpty(value){
+
+    let pattern = /^\s*$/
+    return pattern.test(value);
+
 }
 
-function forwardToId(params) {
-    location.href = "/yObywatel/id?" + params;
+function forwardToId(params){
+
+    location.href = "id?" + params
+
 }
 
 var guide = document.querySelector(".guide_holder");
 guide.addEventListener('click', () => {
-    guide.classList.toggle("unfolded");
-});
+
+    if (guide.classList.contains("unfolded")){
+        guide.classList.remove("unfolded");
+    }else{
+        guide.classList.add("unfolded");
+    }
+
+})
